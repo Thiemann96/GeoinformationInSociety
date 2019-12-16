4859e21c192a1f35c464be7ca0c2d46f15b58eb0
Thiemann96 <e_thie10@uni-muenster.de>
2019-12-17 00:54:00 +0100

    Now with 3d polygon layer of münster

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,15 @@
 import React,{Component,Fragment} from 'react';
 import {StaticMap} from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
+import {PolygonLayer} from '@deck.gl/layers';
 import Overlay from '../Overlays/Overlay'
+import * as data from '../../muenster_buildings.json'
+
+
+const DATA_URL = {
+  BUILDINGS:
+'https://raw.githubusercontent.com/Thiemann96/GeoinformationInSociety/master/src/muenster_buildings.json?token=AELUZUS2BCSKD3AW4R3CQSS6AFI4M'
+};
 
 class Map extends Component {
 
@@ -9,9 +17,11 @@ class Map extends Component {
     super(props)
     this.state = {
       viewState:{
-        longitude: 7.5922197,
-        latitude: 51.9688129,
-        zoom: 13,
+        longitude: 7.615322135118181,
+        latitude: 51.96970534849527,
+        // longitude:51.96582703740133,
+        // latitude:7.622390870898665,
+        zoom: 15,
         pitch: 0,
         bearing: 0
       },
@@ -20,14 +30,35 @@ class Map extends Component {
       mapBoxToken:'pk.eyJ1IjoiZXRoaWUxMCIsImEiOiJjazQyeXlxNGcwMjk3M2VvYmw2NHU4MDRvIn0.nYOmVGARhLOULQ550LyUYA'
     }
   }
+
+  _renderLayers() {
+    const {
+      buildings = DATA_URL.BUILDINGS,
+    } = this.props;
+    return [
+      // This is only needed when using shadow effects
+      new PolygonLayer({
+        id: 'buildings',
+        data: buildings,
+        extruded: true,
+        wireframe: false,
+        opacity: 0.5,
+        getPolygon: f => f.polygon,
+        getElevation: f => f.height,
+        getFillColor: [255, 255, 0]
+      })
+
+    ];
+  }
   
   render() {
-    console.log(this.state.settings)
+
     return (
       <Fragment>
       <DeckGL
         initialViewState={this.state.viewState}
         controller={true}
+        layers={this._renderLayers()}
       >
         <StaticMap   
                 onInter 
