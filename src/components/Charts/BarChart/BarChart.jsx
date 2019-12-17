a682942d7b3270d32eb0dcd7bc3f373e5d44ac6b
sarah37 <sarah@schoettler.email>
2019-12-17 14:48:31 +0100

    fix bar chart

--- src/components/Charts/BarChart/BarChart.jsx
+++ src/components/Charts/BarChart/BarChart.jsx
@@ -1,42 +1,47 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 import * as d3 from "d3";
 
-export default class BarChart extends Component{
+export default class BarChart extends Component {
+	componentDidMount() {
+		this.drawChart();
+	}
+	drawChart() {
+		const data = [12, 5, 6, 6, 9, 10];
 
-    componentDidMount(){
-        this.drawChart();
-    }
-    drawChart() {
-        const data = [12, 5, 6, 6, 9, 10];
-        
-        const svg = d3.select("#numbercyclists")
-        .append("svg")
-        .attr("width", this.props.width)
-        .attr("height", this.props.height)
-        .style("margin-left", 100);
-                      
-        svg.selectAll("rect")
-          .data(data)
-          .enter()
-          .append("rect")
-          .attr("x", (d, i) => i * 70)
-          .attr("y", (d, i) => this.props.height - 10 * d)
-          .attr("width", 65)
-          .attr("height", (d, i) => d * 10)
-          .attr("fill", "green")
+		const barDist = 10;
+		const barWidth =
+			(this.props.width - (data.length - 1) * barDist) / data.length;
 
-          svg.selectAll("text")
-          .data(data)
-          .enter()
-          .append("text")
-          .text((d) => d)
-          .attr("x", (d, i) => i * 70)
-          .attr("y", (d, i) => this.props.height - (10 * d) - 3)
-  
-      }
+		const svg = d3
+			.select("#numbercyclists")
+			.append("svg")
+			.attr("width", this.props.width)
+			.attr("height", this.props.height)
+			.style("margin-left", 100);
 
-    render(){
-        return(
-         <div id={"#" + this.props.id}></div>        )
-    }
+		svg
+			.selectAll("rect")
+			.data(data)
+			.enter()
+			.append("rect")
+			.attr("x", (d, i) => i * (barWidth + barDist))
+			.attr("y", (d, i) => this.props.height - 10 * d)
+			.attr("width", barWidth)
+			.attr("height", (d, i) => d * 10)
+			.attr("fill", "green");
+
+		svg
+			.selectAll("text")
+			.data(data)
+			.enter()
+			.append("text")
+			.text(d => d)
+			.attr("x", (d, i) => i * (barWidth + barDist) + barWidth / 2)
+			.attr("y", (d, i) => this.props.height - 10 * d - 3)
+			.attr("text-anchor", "middle");
+	}
+
+	render() {
+		return <div id={"#" + this.props.id} />;
+	}
 }
