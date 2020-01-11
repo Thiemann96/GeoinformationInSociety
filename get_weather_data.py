aff6537b0e20ed0a92bff1c49c6883075ee90d65
nPhil01 <n_phil01@uni-muenster.de>
2020-01-11 14:14:56 +0100

    Create JSON file with added weather data

--- get_weather_data.py
+++ get_weather_data.py
@@ -3,8 +3,11 @@ import json
 import traceback
 from dwdweather import DwdWeather
 from datetime import datetime
+from tqdm import tqdm
 
+# ID from the station at Muenster/Osnabrueck airport
 dw = DwdWeather()
+station_id = 1766
 
 response = None
 # Connect to database
@@ -14,18 +17,22 @@ try:
 except Exception:
     traceback.print_exc()
 
+# Response not empty
 if response != None:
     data = json.loads(response.text)
+    # Create new JSON and fill it
     weather_data_json = open("weather_data.json","w+")
     weather_data_json.write("[")
     comma = False
-    for accident in data:
-        if accident['lon'] != None and accident['lat'] != None:
+    i = 1
+    for accident in tqdm(data):
+        # Position of accident must be available
+        if accident['lon'] != None and accident['lat'] != None and accident['time_of_day'] != None and accident['date'] != None:
             if comma:
                 weather_data_json.write(",")
             else:
                 comma = True
-            weather_station = dw.nearest_station(lon = accident['lon'], lat = accident['lat'])
+            # Get the accident time and round it to 10 minutes
             accident_time = datetime.strptime(accident['time_of_day'], '%H:%M:%S')
             accident_date = datetime.strptime(accident['date'], '%Y-%m-%d')
             minute = accident_time.minute
@@ -33,9 +40,13 @@ if response != None:
             if minute > 59:
                 minute = 50
             query_hour = datetime(accident_date.year, accident_date.month, accident_date.day, accident_time.hour, minute)
-            result = dw.query(station_id=weather_station["station_id"], timestamp=query_hour)
+            # Query the weather data
+            result = dw.query(station_id=station_id, timestamp=query_hour)
+            # Add weather data to the accident JSON entry
             accident['weather'] = result
+            # Write the data to file
             weather_data_json.write(json.dumps(accident))
-            weather_data_json.write(',')
 
-    weather_data_json.write("];")
+    weather_data_json.write("]")
+else:
+    print("Cannot connect to database or the result is empty")
