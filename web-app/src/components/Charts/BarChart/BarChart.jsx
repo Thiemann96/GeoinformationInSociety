08146b5e45dab99362a4f33e8e590c76f98b2405
sarah37 <sarah@schoettler.email>
2020-01-12 18:31:16 +0100

    Update BarChart.jsx

--- web-app/src/components/Charts/BarChart/BarChart.jsx
+++ web-app/src/components/Charts/BarChart/BarChart.jsx
@@ -19,11 +19,11 @@ export default class BarChart extends Component {
 	}
 
 	drawChart() {
+		// prepare data
 		const data = this.state.accidents
 			.map(d => d.time_of_day)
 			.filter(d => d !== null)
 			.map(d => d.slice(0, 2));
-
 		// nest by hour
 		var nested = d3
 			.nest()
@@ -31,32 +31,51 @@ export default class BarChart extends Component {
 			.entries(data)
 			.map(d => ({ hour: +d.key, count: d.values.length }))
 			.sort((a, b) => a.hour > b.hour);
-
 		console.log(data, nested);
 
-		const barDist = 2;
-		const barWidth =
-			(this.props.width - (nested.length - 1) * barDist) / nested.length;
-		var barHeight = d3
-			.scaleLinear()
-			.domain([0, d3.max(nested, d => d.count)])
-			.range([0, this.props.height]);
+		// set chart margins + dimensions
+		const margin = { left: 40, right: 0, top: 0, bottom: 15 };
+		const dim = {
+			height: this.props.height - margin.top - margin.bottom,
+			width: this.props.width - margin.left - margin.right
+		};
 
+		// create svg
 		const svg = d3
 			.select("#nAccidents")
 			.append("svg")
 			.attr("width", this.props.width)
 			.attr("height", this.props.height);
 
-		svg.selectAll("rect")
+		// g element for bars
+		const g = svg
+			.append("g")
+			.attr(
+				"transform",
+				"translate(" + margin.left + "," + margin.top + ")"
+			);
+
+		// calculate bar dimensions + scaling
+		const barDist = 2;
+		const barWidth =
+			(dim.width - (nested.length - 1) * barDist) / nested.length;
+		var y = d3
+			.scaleLinear()
+			.domain([0, d3.max(nested, d => d.count)])
+			.range([dim.height, 0]);
+
+		// vertical axis
+		g.append("g").call(d3.axisLeft(y));
+
+		g.selectAll("rect")
 			.data(nested)
 			.enter()
 			.append("rect")
 			.attr("x", (d, i) => i * (barWidth + barDist))
-			.attr("y", (d, i) => this.props.height - barHeight(d.count))
+			.attr("y", (d, i) => y(d.count))
 			.attr("width", barWidth)
-			.attr("height", (d, i) => barHeight(d.count))
-			.attr("fill", "green");
+			.attr("height", (d, i) => y(0) - y(d.count))
+			.attr("fill", "steelblue");
 
 		// svg.selectAll("text")
 		// 	.data(data)
