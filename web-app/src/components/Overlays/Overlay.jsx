d8f22b2ee42f7cd9742cda05e17d24f4f6d4b078
Benjamin Karic <b_kari02@uni-muenster.de>
2020-01-21 11:50:09 +0100

    complete manual merge

--- web-app/src/components/Overlays/Overlay.jsx
+++ web-app/src/components/Overlays/Overlay.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, {Component} from "react";
+import {Container, Row, Col} from "react-bootstrap";
 import FilterOverlay from "./FilterOverlay/FilterOverlay";
 import ChartOverlay from "./ChartOverlay/ChartOverlay";
 
@@ -9,14 +9,24 @@ export default class Overlay extends Component {
         this.state = {};
     }
 
-    componentDidMount() {}
+    componentDidMount() {
+    }
 
     render() {
         return (
             <Container className="control-panel">
-                <FilterOverlay _animate={this.props._animate} _resetFilter={this.props._resetFilter} _confirmFilter={this.props._confirmFilter} _toggleHeatMap = {this.props._toggleHeatMap}  _toggleDrawPolygon = {this.props._toggleDrawPolygon} _toggleAccidents = {this.props._toggleAccidents} _toggleBuildings = {this.props._toggleBuildings} filter = {this.props.filter}
-                _handleMapStyle={this.props._handleMapStyle}/>
-                <ChartOverlay   datalength = {this.props.datalength} />
+                <FilterOverlay _animate={this.props._animate} _resetFilter={this.props._resetFilter}
+                               _confirmFilter={this.props._confirmFilter} _toggleHeatMap={this.props._toggleHeatMap}
+                               _toggleAccidents={this.props._toggleAccidents}
+                               _toggleBuildings={this.props._toggleBuildings} filter={this.props.filter}
+                               emptyResult={this.props.emptyResult} _confirmAggregation={this.props._confirmAggregation}
+                               _toggleDrawPolygon = {this.props._toggleDrawPolygon}
+                               _handleMapStyle={this.props._handleMapStyle}
+                               />
+
+                <ChartOverlay datalength={this.props.datalength} accidents={this.props.accidents}
+                              aggregation={this.props.aggregation}
+                              />
 
             </Container>
         );
